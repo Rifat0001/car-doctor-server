248896dfa091007f424c2bedd4d7e646fdddfa00
Rifat0001 <mahmudrifat764@gmail.com>
2023-06-09 02:01:42 +0600

    all done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 // middleware
 app.use(cors());
@@ -22,6 +23,25 @@ const client = new MongoClient(uri, {
     }
 });
 
+// verify the jwt token function
+const verifyJWT = (req, res, next) => {
+    console.log('hitting verify jwt');
+    console.log(req.headers.authorization);
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1];
+    console.log('token inside', token);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +51,17 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         // to store order data 
         const bookingCollection = client.db('carDoctor').collection('bookings');
+
+
+        // jwt token 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1hr'
+            });
+            res.send({ token })
+        })
 
         // get operation to find data in /services route 
         app.get('/services', async (req, res) => {
@@ -46,11 +77,28 @@ async function run() {
             // we don't need all data so we will sort it by following below code 
             const options = {
 
-                projection: { title: 1, price: 1, service_id: 1 }
+                projection: { title: 1, price: 1, service_id: 1, img: 1 }
             };
 
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
+        })
+
+        // to see our booking data in local host 
+        app.get('/bookings/', verifyJWT, async (req, res) => {
+            // console.log(req.headers.authorization);
+            // find order data with email 
+            const decoded = req.decoded;
+            console.log('decoded consoled', decoded)
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result)
         })
 
         // to insert confirm order data in mongodb 
@@ -59,6 +107,30 @@ async function run() {
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
+        })
+
+
+        // to delete service 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // for update any service data 
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+                    status: updatedBooking.status
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
